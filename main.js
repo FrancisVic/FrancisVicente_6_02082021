e00c631e5278b9bc8127bad89e45f34d2d2400f8
Francis Vicente <francivic@outlook.fr>
2021-09-01 15:18:58 +0200

    correction acces tag

--- main.js
+++ main.js
@@ -1,4 +1,19 @@
+async function fetchMovies() {
+    let response = await fetch('FishEyeData.json')
+    console.log(response)
+        .then(function (res) {
+            if (res.ok) {
+                return res.json();
+            }
+        })
+    fetchMovies();
+            .then(function (value) {
+        let photographer = document.getElementsByClassName('photographer')
+        addEventListener('click', photographer)
+        document.createElement('div')
+    })
+        .catch(function (err) {
+            // Une erreur est survenue
+        });
+}
 
-
-// const data = fetchPhotographers("FishEyeData.json");
-// console.log(data);
--- photographers-page.js
+++
@@ -1,8 +0,0 @@
-fetch("FishEyeData.json")
-    .then(function (response) {
-        return response.json();
-    }).then(function (obj) {
-        console.log(obj);
-    }).catch(function (err) {
-        console.error('ERROR');
-    })
