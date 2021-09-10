3404289d2be47e8162942f98121d7d08f2ef44a8
Francis Vicente <francivic@outlook.fr>
2021-09-10 10:00:00 +0200

    deleate role sur le logo

--- main.js
+++ main.js
@@ -1,18 +1,41 @@
-async function fetchPhotographer() {
-    let response = await fetch('FishEyeData.json');
-    console.log(response);
+function fetchPhotograph() {
+    fetch("FishEyeData.json")
+        .then(function (res) {
+            if (res.ok) {
+                return res.json();
+            }
+        })
+        .then(function (value) {
+            generatePhotographer(value);
+            
+
+        })
+        .catch(function (err) {
+            // Une erreur est survenue
+        });
 }
+fetchPhotograph();
 
-document.addEventListener('DOMContentLoaded', async () => {
-    let photographer = [];
-
-    try {
-        photographer = await fetchPhotographer();
+function generatePhotographer(value) { //créer les élements html
+    console.log(value);
+    let str = "https://waytolearnx.com/t.html?name=MimiKheel&age=25&address=paris";
+    let url = new URL(str);
+    let search_params = new URLSearchParams(url.search);
+    if (search_params.has('name')) {
+        let name = search_params.get('name');
+        // console.log(name)
     }
-    catch (e) {
-        console.log('Erreur!');
-        console.log('e');
-    }
-    console.log(photographer);
-});
+    let header = document.getElementById('header');
+    let main = document.getElementById('main');
+    let photographer = document.getElementsByClassName('photographer__picture');
+    photographer.createElement('a');
+    photographer.setAttribute('href');
+    console.log(header);
+    console.log(main);
+    
 
+    
+
+
+    // console.log(value);
+}
