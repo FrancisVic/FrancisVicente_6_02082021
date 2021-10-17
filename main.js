86b9d19ebd094eacab9a5ec0d38f319e6b0e954b
Francis Vicente <francivic@outlook.fr>
2021-10-17 21:08:11 +0200

    Afficher les photographes en fonction de leurs ID

--- main.js
+++ main.js
@@ -18,58 +18,67 @@ fetchPhotograph();
 
 function generatePhotographer(value) {
 
-    
+    let j = 0;
+    while (value.photographers.length > j) {
+        {
+            let photographer = document.createElement('div');
+            photographer.classList.add('photographer');
 
-    
-    console.log(value);
-    let photographer = document.createElement('div');
-    photographer.classList.add('photographer');
+            let containerPicture = document.createElement('div');
+            containerPicture.classList.add('container__photographer__picture');
+            photographer.appendChild(containerPicture);
 
-    let containerPicture = document.createElement('div');
-    containerPicture.classList.add('container__photographer__picture');
-    photographer.appendChild(containerPicture);
+            let photographerPicture = document.createElement('div');
+            photographerPicture.classList.add('photographer__picture');
+            containerPicture.appendChild(photographerPicture);
 
-    let photographerPicture = document.createElement('div');
-    photographerPicture.classList.add('photographer__picture');
-    containerPicture.appendChild(photographerPicture);
+            let imageSrc = document.createElement('img');
+            let a = document.createElement('a');
+            a.setAttribute('href', 'photographer.html?id=' + value.photographers[j].id);
+            imageSrc.setAttribute('src', value.photographers[j].portrait);
 
-    let imageSrc = document.createElement('img');
-    let a = document.createElement('a');
-    a.setAttribute('href', 'photographer.html');
-    imageSrc.setAttribute('src', 'image/EllieRoseWilkens.jpg');
+            photographerPicture.appendChild(a);
+            a.appendChild(imageSrc);
 
-    photographerPicture.appendChild(a);
-    a.appendChild(imageSrc);
+            let getContainerPhotographer = document.getElementById('container__photographer');
 
-    let getContainerPhotographer = document.getElementById('container__photographer');
+            let describe = document.createElement('div');
+            photographer.appendChild(describe);
+            describe.classList.add('photographer__describe');
+            describe.style.fontFamily = 'DM Sans';
+            let namePhotographer = document.createElement('h2');
+            namePhotographer.textContent = value.photographers[j].name;
+            describe.appendChild(namePhotographer);
 
-    let describe = document.createElement('div');
-    photographer.appendChild(describe);
-    describe.classList.add('photographer__describe');
-    describe.style.fontFamily = 'DM Sans';
-    let namePhotographer = document.createElement('h2');
-    namePhotographer.textContent = "Ellie Rose Wilkens";
-    namePhotographer.style.fontWeight = '400';
-    namePhotographer.style.fontSize = '36px';
-    let city = document.createElement('span');
-    city.textContent = "Paris, France";
-    city.style.color = '#901C1C';
+            let city = document.createElement('span');
+            city.classList.add('city');
+            city.textContent = value.photographers[j].city;
+            describe.appendChild(city);
 
-    let priceDescribe = document.createElement('span');
-    priceDescribe.textContent = "Travaille sur des compositions complexes";
+            let priceDescribe = document.createElement('span');
+            priceDescribe.textContent = value.photographers[j].tagline;
+            priceDescribe.classList.add('priceDesc');
+            describe.appendChild(priceDescribe);
 
-    let price = document.createElement('span');
-    price.textContent = "250€/jour";
-    price.style.color = '#757575';
+            let price = document.createElement('span');
+            price.classList.add('price');
+            price.textContent = value.photographers[j].price + "€\/jour";
+            describe.appendChild(price);
 
-    let hashtag = document.createElement('span');
-    hashtag.classList.add('border-tags');
-    hashtag.textContent = "#sport";
-    hashtag.style.fontWeight = '500';
+            getContainerPhotographer.appendChild(photographer);
 
-    describe.appendChild(namePhotographer);
-    describe.appendChild(city);
-    describe.appendChild(price);
-    describe.appendChild(hashtag);
-    getContainerPhotographer.appendChild(photographer);
+            console.log(value.photographers[j]);
+            j++;
+        }
+
+        let i = 0;
+        while (value.photographers[j].tags.length > i) {
+            let hashtag = document.createElement('span');
+            hashtag.classList.add('border-tags');
+            hashtag.textContent = value.photographers[j].tags[i];
+            console.log(value.photographers[j].tags[i]);
+            describe.appendChild(hashtag);
+            i++;
+        }
+    }
 }
